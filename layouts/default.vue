b5a985c1f10d3b078de70a6c20ab8302b0bf88de
Jorge Mario Mesa <jorgemesa20@hotmail.com>
2023-04-01 18:08:09 -0500

    Pagina base

---
+++ layouts/default.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      :absolute="!fixed" app
+      
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar :clipped-left="clipped" app centered>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-account-question',
+          title: 'Personajes',
+          to: '/personajes',
+        },
+        {
+          icon: 'mdi-newspaper-variant-multiple-outline',
+          title: 'Comics',
+          to: '/comics',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'MARVEL',
+    }
+  },
+}
+</script>
